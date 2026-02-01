52c57e366140998fcc9e7173ba0906c68bf9d9d8
Solanki Mahesh <Solanki777>
2026-02-01 15:24:02 +0530

    humancare

--- patient_appointments.php
+++ patient_appointments.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'config/config.php';
 require_once 'config/database.php';
+require_once 'includes/session.php';
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
